4092cd1f19f69c93005fc8c978150ea064bdd00b
Andre <andreivanp@gmail.com>
2022-11-24 20:06:07 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hai Dunia</h1>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
